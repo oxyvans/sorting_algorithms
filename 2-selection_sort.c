b7c630f03dc4a36d7b3cfe6587e32a1035f1a95b
oxyvans <benja.1999.g@hotmail.com>
2022-06-13 05:38:01 -0700

    add task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,7 +21,9 @@ void selection_sort(int *array, size_t size)
 				min = j;
 		}
 		if (min != i)
+		{
 			swap(&array[min], &array[i]);
-		print_array(array, size);
+			print_array(array, size);
+		}
 	}
 }
